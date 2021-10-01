91600a1bbd0c16692ca72763964cd837cbcb966b
claudio <claudio@tech.techadsmedia.com>
2021-10-01 16:06:39 +0200

    log in file option

--- mailfetcher.cpp
+++ mailfetcher.cpp
@@ -1,15 +1,86 @@
 #include "mailfetcher.h"
 #include "QStacker/qstacker.h"
+#include "fileFunction/filefunction.h"
+#include "magicEnum/magic_from_string.hpp"
 #include "minCurl/mincurl.h"
 #include <QDebug>
 #include <curl/curl.h>
+
+void MailFetcher::logSearchQuery(QString username, QString password, QString folderUrl, QString searchQuery) {
+	if (!logExecution) {
+		return;
+	}
+
+	QString skel = R"(
+- request -
+username = %1 password = %2 folderUrl = %3 searchQuery = %4
+)";
+	auto    msg  = skel
+				   .arg(username)
+				   .arg(password)
+				   .arg(folderUrl)
+				   .arg(searchQuery);
+	logWithTime(logFile, msg);
+}
+
+void MailFetcher::logSearchResponse(QString curlCode, QString curlError, QString result) {
+	if (!logExecution) {
+		return;
+	}
+
+	QString skel = R"(
+- response -
+curlCode = %1 curlError = %2 result = %3
+)";
+	auto    msg  = skel
+				   .arg(curlCode)
+				   .arg(curlError)
+				   .arg(result);
+	logWithTime(logFile, msg);
+}
+
+void MailFetcher::logMailQuery(QString mailUrl) {
+	if (!logExecution) {
+		return;
+	}
+
+	QString skel = R"(
+- request -
+mailUrl = %1
+)";
+	auto    msg  = skel
+				   .arg(mailUrl);
+	logWithTime(logFile, msg);
+}
+
+void MailFetcher::logMailResponse(QString curlCode, QString curlError, QString mail) {
+	if (!logExecution) {
+		return;
+	}
+
+	QString skel = R"(
+- response -
+curlCode = %1 curlError = %2 mail (truncated) = %3
+)";
+	auto    msg  = skel
+				   .arg(curlCode)
+				   .arg(curlError)
+				   .arg(mail.left(50));
+	logWithTime(logFile, msg);
+}
 
 Mail MailFetcher::fetch(bool verbose, bool printError) {
 	CurlKeeper     curl;
 	CURLcode       res = CURLE_OK;
 	CurlCallResult result;
 	Mail           emptyMail;
-	if (curl) { /* Set username and password */
+	if (curl) {
+		/*
+		 * search query
+		 * find the id of the mail we want to download
+		 */
+		logSearchQuery(username, password, folderUrl, searchQuery);
+
 		curl_easy_setopt(curl, CURLOPT_USERNAME, username.toLocal8Bit().data());
 		curl_easy_setopt(curl, CURLOPT_PASSWORD, password.toLocal8Bit().data());
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result.result);
@@ -17,6 +88,10 @@ Mail MailFetcher::fetch(bool verbose, bool printError) {
 		curl_easy_setopt(curl, CURLOPT_URL, folderUrl.toLocal8Bit().data());
 		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, searchQuery.toLocal8Bit().data()); /* Perform the fetch */
 		res = curl_easy_perform(curl);
+
+		// response
+		auto& r = result.result;
+		logSearchResponse(asString(res), curl_easy_strerror(res), r);
 
 		// check curl result
 		if (res != CURLE_OK) {
@@ -29,7 +104,6 @@ Mail MailFetcher::fetch(bool verbose, bool printError) {
 			return emptyMail;
 		}
 
-		auto& r = result.result;
 		r.replace("* SEARCH", "");
 		r         = r.trimmed();
 		auto list = r.split(' ');
@@ -40,17 +114,25 @@ Mail MailFetcher::fetch(bool verbose, bool printError) {
 			return emptyMail;
 		case 1: {
 			// 1 mail
+
+			/*
+			 * mail query
+			 */
 			auto mailId  = list[0].toInt();
 			auto mailUrl = QSL("%1;UID=%2")
 							   .arg(folderUrl)
 							   .arg(mailId);
 
+			logMailQuery(mailUrl);
 			curl_easy_setopt(curl, CURLOPT_URL, mailUrl.toLocal8Bit().data());
 			curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, NULL);
 			CurlCallResult mailResult;
 			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &mailResult.result);
 			res = curl_easy_perform(curl);
 
+			// response
+			auto mail = mailResult.result;
+			logMailResponse(asString(res), curl_easy_strerror(res), mail);
 			// check curl result
 			if (res != CURLE_OK) {
 				if (printError) {
@@ -59,7 +141,6 @@ Mail MailFetcher::fetch(bool verbose, bool printError) {
 				return emptyMail;
 			}
 
-			auto mail = mailResult.result;
 			if (verbose) {
 				qDebug().noquote() << mail;
 			}
--- mailfetcher.h
+++ mailfetcher.h
@@ -9,6 +9,9 @@ struct MailFetcherConfig {
 	// imap query to search the mail that will be downloaded
 	QString searchQuery;
 	QString folderUrl;
+
+	bool    logExecution = false;
+	QString logFile;
 };
 
 struct Mail {
@@ -23,13 +26,24 @@ class MailFetcher {
 	QString searchQuery;
 	QString folderUrl;
 
+	bool    logExecution = false;
+	QString logFile;
+
 	MailFetcher(MailFetcherConfig conf) {
-		this->username    = conf.username;
-		this->password    = conf.password;
-		this->searchQuery = conf.searchQuery;
-		this->folderUrl   = conf.folderUrl;
+		this->username     = conf.username;
+		this->password     = conf.password;
+		this->searchQuery  = conf.searchQuery;
+		this->folderUrl    = conf.folderUrl;
+		this->logExecution = conf.logExecution;
+		this->logFile      = conf.logFile;
 	}
 	Mail fetch(bool verbose, bool printError);
+
+	  private:
+	void logSearchQuery(QString username, QString password, QString folderUrl, QString searchQuery);
+	void logSearchResponse(QString curlCode, QString curlError, QString result);
+	void logMailQuery(QString mailUrl);
+	void logMailResponse(QString curlCode, QString curlError, QString mail);
 };
 
 #endif // MAILFETCHER_H
